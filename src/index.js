d16593a548e0b599d32c7471e9cd79d83f92f70f
lak0909 <lang0909@naver.com>
2020-11-08 17:25:16 +0900

    add entry files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'MY FO4 VERSION REACT'
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById("root")
+)
