f2fc28759c61523c0dd3dae3cbf60ade85cedbb2
lak0909 <lang0909@naver.com>
2020-11-14 19:58:31 +0900

    add redux package and store setting and use redux with hook

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { SearchAction } from '../actions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Search = () => {
+    const search = useSelector(state => state.search.search)
+    const dispatch = useDispatch()
+
     return (
-        <div className="home">
-            <div className="logo_cont" style={{textAlign: 'center', marginTop: '20px'}}>
+        <div className="home" style={{textAlign: 'center'}}>
+            <div className="logo_cont" style={{marginTop: '20px'}}>
                 <img 
                     className="logo" 
                     src="http://ec2-54-180-32-236.ap-northeast-2.compute.amazonaws.com:3000/img/my_background.74e59402.jpg"
@@ -14,6 +19,17 @@ const Search = () => {
                         height: 'auto',
                     }}
                 />
+            </div>
+            <div className="bp3-input-group bp3-round" style={{maxWidth: '400px', height: '30px', margin: '10px auto'}}>
+                <span className="bp3-icon bp3-icon-search"></span>
+                <input 
+                    type="text" 
+                    className="bp3-input" 
+                    placeholder="선수명을 입력해주세요." 
+                    value={search}
+                    onChange={(e) => dispatch(SearchAction.change(e.target.value))}
+                />
+                <button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-arrow-right"></button>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import 'normalize.css/normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
-const title = 'MY FO4 VERSION REACT'
-
 ReactDOM.render(
-    <App></App>,
+    <Provider store={store}>
+        <App></App>
+    </Provider>,
     document.getElementById("root")
 )
