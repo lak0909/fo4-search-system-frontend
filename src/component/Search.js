cab5b1f8b8a1061d13db44600a2066cd216bd15b
lak0909 <lang0909@naver.com>
2020-11-14 14:27:43 +0900

    add background image and npm package

--- src/component/Search.js
+++ src/component/Search.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div>
-            Search Page
+        <div className="home">
+            <div className="logo_cont" style={{textAlign: 'center', marginTop: '20px'}}>
+                <img 
+                    className="logo" 
+                    src="http://ec2-54-180-32-236.ap-northeast-2.compute.amazonaws.com:3000/img/my_background.74e59402.jpg"
+                    style={{
+                        width: '100%',
+                        maxWidth: '650px',
+                        maxHeight: '370px',
+                        height: 'auto',
+                    }}
+                />
+            </div>
         </div>
     )
 }
--- src/layout/Top.js
+++ src/layout/Top.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Top = () => {
-    const history = useHistory()
     return (
         <nav className="bp3-navbar bp3-dark">
             <div className="bp3-navbar-group bp3-align-left">
