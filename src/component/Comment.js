29a6a46042168742cbf4fdb8d0ef9b6de30899b8
lak0909 <lang0909@naver.com>
2020-12-06 22:21:37 +0900

    add comment register and add toaster

--- src/component/Comment.js
+++ src/component/Comment.js
@@ -1,16 +1,45 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import BarChart from './BarChart'
 import PlayerCard from './PlayerCard'
+import axios from 'axios'
+import { SearchAction } from '../actions'
+import { MyToaster } from './MyToaster'
+import { Intent } from '@blueprintjs/core'
 
 const Comment = ({ match }) => {
 
-    const { selectedData, selected, comment } = useSelector(state => state.search)
+    const { selectedData, selected, comment, commentParam } = useSelector(state => state.search)
+    const [text,setText] = useState('')
+    const dispatch = useDispatch()
     const data = selectedData.filter(val => {
         if(val.spPosition == match.params.position){
             return val
         }
     })
+
+    const handleClick = () => {
+        axios.post(`http://ec2-13-209-80-204.ap-northeast-2.compute.amazonaws.com:3000/top_record/${commentParam}/comment`,{
+            content: text
+        }).then(res => {
+            if(res.status === 200){
+                MyToaster.show({
+                    message: "등록되었습니다.",
+                    intent: Intent.SUCCESS,
+                    timeout: 2000
+                })
+            }
+        }).catch(error => {
+            MyToaster.show({
+                message: "등록실패했습니다.",
+                intent: Intent.DANGER,
+                timeout: 2000
+            })
+        }).finally(() => {
+            dispatch(SearchAction.setCommentParam(commentParam))
+        })
+    }
+
     return (
         <div className="bp3-card bp3-elevation-1" style={{ width: '100%', maxWidth: '950px', margin: '50px auto', overflow: 'auto', textAlign: 'center' }}>
             <div>
@@ -38,8 +67,13 @@ const Comment = ({ match }) => {
                 <div className="bp3-card bp3-elevation-1" style={{ margin: '1px auto', display: 'flex' }}>
                     <PlayerCard value={selected}></PlayerCard>
                     <div style={{ display: 'flex', flexDirection: 'column', marginTop: '10px', width: '100%', marginBottom: '10px' }}>
-                        <textarea className="bp3-input bp-small bp3-intent-primary bp3-fill" dir="auto"></textarea>
-                        <button type="button" className="bp3-button bp3-icon-add bp3-intent-primary">등록</button>
+                        <textarea 
+                            className="bp3-input bp-small bp3-intent-primary bp3-fill" 
+                            dir="auto"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                        <button type="button" className="bp3-button bp3-icon-add bp3-intent-primary" onClick={handleClick}>등록</button>
                     </div>
                 </div>
             </div>
---
+++ src/component/MyToaster.js
@@ -0,0 +1,6 @@
+import { Position, Toaster } from '@blueprintjs/core'
+
+export const MyToaster = Toaster.create({
+    className: "my-toaster",
+    position: Position.TOP
+})
